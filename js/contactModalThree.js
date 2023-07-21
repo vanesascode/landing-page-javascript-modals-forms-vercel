170d23c7e9aba9b7399c60c960bf72b1abcc9319
vanesascode <vanesa.juarez.paris@gmail.com>
2023-07-21 11:44:12 +0200

    vanesascode

--- js/contactModalThree.js
+++ js/contactModalThree.js
@@ -3,35 +3,35 @@
 
 //STEP 1 --- Open the model by clicking the 'Contact us button' (and being able to close it: )
 
-function openModal() {
-  const contactModalContainer = document.getElementById("contactModalContainer");
-  contactModalContainer.style.display = "block";
+function openModalThree() {
+  const contactModalContainerThree = document.getElementById("contactModalContainerThree");
+  contactModalContainerThree.style.display = "block";
 }
 
-const contactModalButtonmodalButton = document.getElementById("contactModalButton");
-contactModalButton.addEventListener("click", openModal);
+const contactModalLink = document.getElementById("contactModalLink");
+contactModalLink.addEventListener("click", openModalThree);
 
-function closeModalTwo() {
-  contactModalContainer.style.display = "none";
+function closeModalThree() {
+  contactModalContainerThree.style.display = "none";
 }
 
 //We could use the  'onclick'  attribute directly in HTML, which can be convenient for simple and straightforward interactions. However, using event listeners in JavaScript provides more flexibility and control over your code. It allows you to separate the HTML and JavaScript logic, making your code more organized and easier to maintain.
 
 //STEP 2 --- select elements from modal and form:
 
-let formTwo = document.querySelector('.form-two');
-let modalTwo = document.querySelector('.modal-two');
+let formThree = document.querySelector('.form-three');
+let modalThree = document.querySelector('.modal-three');
 
 
 // //STEP 3: Attach the function 'closeFormTwo' to the form submit event. This means that when the form is submitted, the 'closeFormTwo' function will be executed.
 
 
-formTwo.onsubmit = closeFormTwo;
+formThree.onsubmit = closeFormThree;
 
 // //STEP 3: Declare the function 'closeFormTwo'. It closes the modal:
 
-function closeFormTwo() {
-  modalTwo.style.display = "none";
+function closeFormThree() {
+  modalThree.style.display = "none";
 }
 
 
